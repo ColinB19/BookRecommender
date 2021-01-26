254d2fbebea519852c237df5d4edb6911e905049
ColinB19 <c.bradley.do@gmail.com>
2021-01-25 21:46:02 -0800

    implemented a random search for hyperparameter tuning. Changed around the class definitions.

--- recPipeline.py
+++ recPipeline.py
@@ -6,18 +6,22 @@ import pandas as pd
 import random
 from scipy.sparse import coo_matrix as cm
 import lightfm as lf
+from lightfm.evaluation import precision_at_k, recall_at_k, auc_score
 
 # table formatting
 from IPython.display import display, HTML
 
+# for progress bars
+from tqdm import tqdm
+
 class Pipeline():
 
     def __init__(self):
-        books = pd.DataFrame()
-        ratings = pd.DataFrame()
-        book_tags = pd.DataFrame()
-        tags = pd.DataFrame()
-        to_read = pd.DataFrame()
+        self.books = pd.DataFrame()
+        self.ratings = pd.DataFrame()
+        self.book_tags = pd.DataFrame()
+        self.tags = pd.DataFrame()
+        self.to_read = pd.DataFrame()
 
     def preprocess(self):
         '''
@@ -32,27 +36,6 @@ class Pipeline():
         '''
         self.read_data()
         self.fix_ids()
-
-    def get_model(self, epochs=20, num_threads=2, loss='warp'):
-        '''
-        This is just a clean way to get the model. Creates interaction sparse matrices
-        and then trains a model on them. Note: right now this is purely collaborative filtering,
-        LightFM supports Hybrid filtering which we will incorporate in the next version.
-
-        input: 
-            self - class object.
-            epochs - number of epochs for gradient descent.
-            num_threads - number of physical cores to parallelize over.
-            loss - loss function type. See LightFM Documentation. 
-
-        output: 
-            model - our trained model.
-        '''
-        interactions = self.get_interactions()
-        # user_meta, item_meta = self.get_meta()
-        model = self.fit_model(interactions, epochs, num_threads, loss)
-
-        return model
     
     
     def read_data(self):
@@ -104,16 +87,25 @@ class Pipeline():
         self.book_tags['iid'] = self.book_tags.goodreads_book_id.map(idMAP)
         self.book_tags.drop('goodreads_book_id', inplace=True,axis = 1)
 
-    def get_interactions(self):
+    def get_interactions(self, ratingsThresh = 0):
         '''
-        This function gets the interactions sparse matrix using a scipy sparse coo_matrix.
+        This function gets all of the interactions sparse matrix using a scipy sparse coo_matrix.
 
-        inputs:
-            self - class object
+        Parameters
+        ----------
+        ratingsThresh : int, optional
+            If you only want to include ratings over a certain number. The default is 0.
 
-        outputs:
-            ratSparse - sparse interactions matrix. 
+        Returns
+        -------
+        ratSparse : TYPE
+            DESCRIPTION.
+
         '''
+        
+        # only look at ratings above ratingsThresh
+        self.ratings = self.ratings.query('rating >= @ratingsThresh')
+        
         numUsers = self.ratings.uid.max()+1
         numBooks = self.ratings.iid.max()+1
 
@@ -121,34 +113,177 @@ class Pipeline():
                        shape=(numUsers, numBooks))
         return ratSparse
 
+    def split_test_train(self,
+                         testTrainFrac = 0.5, 
+                         ratingsWithheldFrac = 0.4,
+                         testValFrac = 0.5,
+                         ratingsThresh = 0):
+        '''
+        This function takes in the entire ratings dataset and splits the interaction matrices into a training, testing and validation set.
+        First the function splits off a portion of reviews that are then split up into test and validation sets. This function is different
+        from scikit-learn in that it preserves all user-ids in the training set to avoid a cold start problem when testing your algorithm. 
+
+        Parameters
+        ----------
+        testTrainFrac : flaot, optional
+            What percentage of the users would you like to test/validate on. The default is 0.5.
+        ratingsWithheldFrac : float, optional
+            What percentage of the ratings of the test/val users would you like to withhold. The default is 0.4.
+        testValFrac : float, optional
+            Fraction to split test into test/validation. The default is 0.5.
+        ratingsThresh : int, optional
+            If you only want to include ratings over a certain number. The default is 0.
+
+        Returns
+        -------
+        sparseTrain : scipy coo matrix
+            A sparse matrix of the training user-item interactions. 
+        sparseTest : scipy coo matrix
+            A sparse matrix of the testing user-item interactions. 
+        sparseVal : scipy coo matrix
+            A sparse matrix of the validation user-item interactions. 
+        '''
+        import random
+        random.seed(1001)
+
+
+        # only look at ratings above ratingsThresh
+        self.ratings = self.ratings.query('rating >= @ratingsThresh')
+        
+        # let's get the list of users for the test set
+        uids = self.ratings.uid.unique().tolist()
+        test_uids = random.sample(uids, k = int(len(uids)*testTrainFrac))
+
+        # get all data for test users and put the rest in training
+        test_users = self.ratings.query('uid == @test_uids')
+        train = self.ratings.query('uid != @test_uids')
+
+        # only consider 40% of test users ratings and put the rest in the train set.
+        # This prevents the cold start problem on the test set. We will later incorporate 
+        # new users. 
+        test = test_users.groupby('uid').sample(frac = ratingsWithheldFrac, random_state = 1)
+        validation = test.groupby('uid').sample(frac = testValFrac, random_state = 888)
+        train = train.append(test_users.drop(test.index), ignore_index = True)
+        test.drop(validation.index,inplace = True)
+
+        # get these into sparse matrices
+        trainNumUsers = train.uid.max()+1
+        trainNumBooks = train.iid.max()+1
+
+        sparseTrain = cm((train.rating, (train.uid, train.iid)),
+                             shape=(trainNumUsers, trainNumBooks))
+
+        testNumUsers = test.uid.max()+1
+        testNumBooks = test.iid.max()+1
+
+        sparseTest = cm((test.rating, (test.uid, test.iid)),
+                            shape=(testNumUsers, testNumBooks))
+
+        valNumUsers = validation.uid.max()+1
+        valNumBooks = validation.iid.max()+1
+
+        sparseVal = cm((validation.rating, (validation.uid, validation.iid)),
+                               shape=(valNumUsers, valNumBooks))
+        
+        return sparseTrain, sparseTest, sparseVal
+        
 #     def get_meta(self):
 #         numUsers = self.ratings.uid.max()+1
 #         numBooks = self.ratings.iid.max()+1
 #         numUserFeatures = self.to_read.book_id.max+1
 #         numItemFeatures = self.tags.book_id.max+1
 
-    def fit_model(self, ratSparse, epochs, num_threads, loss):
+
+class Recommender():
+    
+    def __init__(self):
+        self.model = lf.LightFM(loss = 'warp')
+    
+    def fit(self,
+            interactions,
+            no_components = 10,
+            learning_schedule = 'adagrad',
+            loss='warp',
+            learning_rate = 0.05,
+            item_alpha = 0.0,
+            user_alpha = 0.0,
+            max_sampled = 10,
+            epochs=20, 
+            num_threads=2):
         '''
-        This gets a LightFM model and trains it on the sparse matrix.
+        this function trains our SVD model. 
 
-        inputs:
-            self - class object.
-            ratSparse - sparse interactions matrix.
-            epochs - number of epochs for gradient descent.
-            num_threads - number of physical cores to parallelize over.
-            loss - loss function type. See LightFM Documentation. 
+        Parameters
+        ----------
+        interactions : coo_sparse matrix
+            sparse user-items interaction training matrix
+        rest : see LightFM documentation.
+               https://making.lyst.com/lightfm/docs/lightfm.html
 
-        outputs:
-            model - our trained model.
+        Returns
+        -------
+        None.
 
         '''
+        # user_meta, item_meta = self.get_meta()
+        self.model = lf.LightFM(no_components = no_components,
+                                learning_schedule = learning_schedule,
+                                loss=loss,
+                                learning_rate = learning_rate,
+                                item_alpha = item_alpha,
+                                user_alpha = user_alpha,
+                                max_sampled = max_sampled)
+        self.model.fit(interactions, epochs=epochs, num_threads=num_threads)
 
-        model = lf.LightFM(loss=loss)
-        model.fit(ratSparse, epochs=epochs, num_threads=num_threads)
+    def paramSearch(self, train, test, num_samples = 10, num_threads = 2):
+        '''
+        This function trains models on various sets of random hyperparameters
+        and returns a dataframe with scores. 
+        
+        Parameters
+        ----------
+        interactions : coo_sparse matrix
+            sparse user-items interaction training matrix
+        num_samples : int
+            How many random samples do you want to pull? The default is 10
+        num_threads : int
+            number of threads to parallelize over. the default is 2.
 
-        return model
+        Returns
+        -------
+        hyperparameters with metric scores.
 
-    def recommend_random(self, seed, model):
+        '''
+        hyperparams = pd.DataFrame()
+        print('Searching for optimal parameters...')
+        for i in tqdm(range(num_samples)):
+            # get a random sample of hyperparameters
+            params = sample_hyperparameters()
+
+            # train a model using those hyperparameters
+            self.fit(train, **params, num_threads = num_threads)
+
+            prec = precision_at_k(self.model,
+                                  test_interactions = test,
+                                  train_interactions = train,
+                                  num_threads=num_threads)
+            rec = recall_at_k(self.model,
+                              test_interactions = test,
+                              train_interactions = train,
+                              num_threads=num_threads)
+            auc = auc_score(self.model,
+                            test_interactions = test,
+                            train_interactions = train,
+                            num_threads=num_threads)
+
+            params['precision_mean'] = prec.mean()
+            params['recall_mean'] = rec.mean()
+            params['auc_mean'] = auc.mean()
+
+            hyperparams = hyperparams.append(params, ignore_index=True).sort_values(by = 'precision_mean')
+        return hyperparams
+        
+    def recommend_random(self, ratings, books, seed):
         '''
         This function will pick a random user out of the list of known users and print out their top 
         10 recommendations! It will also print their top 10 rated items for comparison.
@@ -163,17 +298,17 @@ class Pipeline():
         '''
 
         random.seed(seed)
-        user = random.choices(self.ratings.uid.unique().tolist())[0]
+        user = random.choices(ratings.uid.unique().tolist())[0]
 
         # now let's predict them on our trained model
-        itemList = np.array(self.ratings.iid.unique().tolist())
+        itemList = np.array(ratings.iid.unique().tolist())
         itemList.sort()
 
-        knownRatings = pd.merge(self.ratings.query('uid == @user'),
-                                self.books[['iid', 'title', 'authors']], on='iid', how='left')
+        knownRatings = pd.merge(ratings.query('uid == @user'),
+                                books[['iid', 'title', 'authors']], on='iid', how='left')
 
-        score = model.predict(user, itemList)
-        suggested = self.books.loc[np.argsort(-score)][['title', 'authors']]
+        score = self.model.predict(user, itemList)
+        suggested = books.loc[np.argsort(-score)][['title', 'authors']]
 
         print(color.BOLD + 'User {} known items: '.format(user) + color.END, end='\n')
         display(HTML(knownRatings[['title', 'authors', 'rating']]
@@ -181,7 +316,7 @@ class Pipeline():
         print(color.BOLD + 'Top 10 suggested items:' + color.END, end='\n')
         display(HTML(suggested[:10].to_html()))
         
-        
+                
         
         
 class color:
@@ -197,4 +332,20 @@ class color:
     RED = '\033[91m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
-    END = '\033[0m'        
+    END = '\033[0m'  
+
+def sample_hyperparameters():
+    """
+    Yield possible hyperparameter choices.
+    """
+
+    return {
+        "no_components": np.random.randint(5, 64),
+        "learning_schedule": np.random.choice(["adagrad", "adadelta"]),
+        "loss": np.random.choice(["bpr", "warp", "warp-kos"]),
+        "learning_rate": np.random.exponential(0.05),
+        "item_alpha": np.random.exponential(1e-8),
+        "user_alpha": np.random.exponential(1e-8),
+        "max_sampled": np.random.randint(5, 15),
+        "epochs": np.random.randint(5, 50),
+        }      
---
+++ test.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 25 17:36:14 2021
+
+@author: colin
+"""
+import recPipeline as pipe
+import pandas as pd
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
+
+check = pipe.Pipeline()
+
+check.preprocess()
+train, test, val = check.split_test_train(testTrainFrac = 1, ratingsWithheldFrac= 0.2, ratingsThresh=4)
+
+model = pipe.Recommender()
+hyper = model.paramSearch(train = train, test = test, num_samples=2, num_threads=20)
+
+
+#%%
+
+model.recommend_random(check.ratings, check.books, seed = 1234)
